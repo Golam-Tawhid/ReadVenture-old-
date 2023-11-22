d337feec3d7c4feac928a1c4088935bbe5bb28a2
Golam Tawhid <tawhidfahad199@gmail.com>
2023-11-22 23:13:25 +0600

    home renamed

--- bookswap/readventure/urls.py
+++ bookswap/readventure/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('congratulations/', views.congratulations, name='congratulations'),
+    path('home/', views.home, name='home'),
 ]
--- bookswap/readventure/views.py
+++ bookswap/readventure/views.py
@@ -7,25 +7,22 @@ def index(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(f"Username: {username}, Password: {password}")
-        
         user = authenticate(request, username=username, password=password)
-        print(user)
 
         if user is not None:
             login(request, user)
             request.session['login_success'] = True
-            return redirect('congratulations')  # Redirect to the congratulations page
+            return redirect('home')  # Redirect to the congratulations page
         # else:
         #     # Handle invalid login credentials
         #     return render(request, 'readventure/index.html', {'error_message': 'Invalid login credentials'})
 
     return render(request, 'readventure/index.html')
 
-def congratulations(request):
+def home(request):
     if request.session.get('login_success'):
         del request.session['login_success']  # Remove the session variable to avoid showing the message on page refresh
-        return render(request, 'readventure/congratulations.html')
+        return render(request, 'readventure/home.html')
     else:
         # Redirect to the login page if there's no successful login session
         return redirect('index')  # Update 'user_login' with the actual name of your login view
