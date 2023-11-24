ea1d2b08c8c110d4128493e8f5ef8c953fda4d71
Golam Tawhid <tawhidfahad199@gmail.com>
2023-11-24 15:12:01 +0600

    update for vercel deployment

--- bookswap/bookswap/settings.py
+++ bookswap/bookswap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p7$fkjb)vbqlls01^4zu4-psyb+zy+hp&#j-^vw2wra9a=+1v#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
